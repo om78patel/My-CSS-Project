a325a5abeadbcfccc476d378170c12ef43c5613c
om78patel <137567070+om78patel@users.noreply.github.com>
2024-01-11 19:07:54 -0500

    commit 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Load header
+    $('#global-header').load('header.html', function() {
+        // Header loaded callback, if needed
+    });
+
+    // Load footer
+    $('#global-footer').load('footer.html', function() {
+        // Footer loaded callback, if needed
+    });
+    
+    // 'js/mian.js'
+
+});
